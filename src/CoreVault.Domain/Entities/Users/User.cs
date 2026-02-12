e0175c48d36f15169994fa89d15924b6fb910c69
Matheus Ribeiro <omatheusribeiro@outlook.com>
2026-02-11 22:43:05 -0300

    All the necessary entities have been created to register, view, and edit the platform's data.

---
+++ src/CoreVault.Domain/Entities/Users/User.cs
@@ -0,0 +1,22 @@
+﻿using CoreVault.Domain.Entities.Base;
+
+namespace CoreVault.Domain.Entities.Users
+{
+    public class User : EntityBase
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password {  get; set; } = string.Empty;
+
+        private User() { }
+
+        public User(string firstName, string lastName, string email, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Password = password;
+        }
+    }
+}
---
+++ src/CoreVault.Domain/Entities/Vaults/Vault.cs
@@ -0,0 +1,21 @@
+﻿using CoreVault.Domain.Entities.Base;
+using CoreVault.Domain.Entities.Credentials;
+
+namespace CoreVault.Domain.Entities.Vaults
+{
+    public class Vault : EntityBase
+    {
+        public string Name { get; private set; } = string.Empty;
+        public int UserId { get; private set; }
+        private readonly List<Credential> _credentials = new();
+        public IReadOnlyCollection<Credential> Credentials => _credentials;
+
+        private Vault() { }
+
+        public Vault(string name, int userId)
+        {
+            Name = name;
+            UserId = userId;
+        }
+    }
+}
